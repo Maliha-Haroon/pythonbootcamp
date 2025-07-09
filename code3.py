249b53f73fd7d038d47f35e4fb2eda28ab2f1e39
Maliha-Haroon <165473947+Maliha-Haroon@users.noreply.github.com>
2025-07-09 18:47:42 +0500

    Add files via upload

---
+++ code3.py
@@ -0,0 +1,35 @@
+#Arithmetic
+a = 7
+b = 4
+c = a + b
+print(c)
+
+d = a*b
+print(d)
+
+# Assignment 
+a = 4-2
+print(a)
+
+b= 6
+b += 3
+print(b)
+
+c  -=3
+print(c)
+
+#comparision operator
+f = 5<4
+print(f) 
+
+g = 5<=5
+print(g)
+
+print(5 == 6)
+
+#LOGICAL OPERATORS
+e = True or False
+print(e)
+
+
+
